94366aa3fcc6e2838d163bedfa13eaa54a24bd41
Cyril kurillos <bocage.cyril@gmail.com>
2026-03-05 16:27:50 +0100

    Ajout de la logique d'envoi de mail

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,5 @@
+<tr>
+<td class="content-cell" align="center">
+    <p>© {{ date('Y') }} Rachelle Conception Visuelle et Photographie - Tous droits réservés.</p>
+</td>
+</tr>
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,12 @@
+@props(['url'])
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="https://rachelle-art-visuels-storage.s3.eu-north-1.amazonaws.com/logo_public/logo.png" 
+                class="logo" 
+                alt="Rachelle Arts" 
+                width="200"
+            >
+        </a>
+    </td>
+</tr>
