daff0d3461d1e74e42cd3bc64dc24aacb39d0ad6
Cyril <cyril@MacBook-Pro-de-Rachelle.local>
2026-06-05 10:08:33 +0200

    Build assets - stats recharts

---
+++ public/build/assets/index-qtB-h4Qu.js
@@ -0,0 +1,1 @@
+import{b as r}from"./react-vendor-Y7T1HomI.js";var a=r();export{a as r};
---
+++ public/build/assets/message-square-i1N6HPhX.js
@@ -0,0 +1,11 @@
+import{c as e}from"./createLucideIcon-Cn1Wg9O6.js";/**
+ * @license lucide-react v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],c=e("chevron-left",a);/**
+ * @license lucide-react v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M22 17a2 2 0 0 1-2 2H6.828a2 2 0 0 0-1.414.586l-2.202 2.202A.71.71 0 0 1 2 21.286V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2z",key:"18887p"}]],t=e("message-square",o);export{c as C,t as M};
